0589ec991c5906c95e641b99e1f67526137723e2
Erica <159991570+Am-erica@users.noreply.github.com>
2026-09-16 04:04:35 +0000

    Finalizando

--- src/main/java/br/edu/mackenzie/gerenciadornomes/GerenciadorNomesApplication.java
+++ src/main/java/br/edu/mackenzie/gerenciadornomes/GerenciadorNomesApplication.java
@@ -20,19 +20,14 @@ public class GerenciadorNomesApplication implements CommandLineRunner {
     public void run(String... args) {
 
         // Conexao com banco de dados
-        String url = "jdbc:h2:file:./data/banco_dados";
-        String usuario = "admin";
-        String senha = "admin";
+        String url = "jdbc:postgresql://aws-0-us-west-2.pooler.supabase.com:5432/postgres?sslmode=require";
+        String usuario = "postgres.fchpfhxyfpyrlcmdharr";
+        String senha = "Senha"; 
 
         try (Connection connection =
                 DriverManager.getConnection(url, usuario, senha);
             Statement statement = connection.createStatement()) {
 
-            statement.execute("""
-                CREATE TABLE IF NOT EXISTS nomes (
-                    nome VARCHAR(256) NOT NULL UNIQUE
-                )
-                """);
             
             codigoAnterior(connection);
 
